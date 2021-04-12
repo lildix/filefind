a33e56b97c6108e94163e2916e3ade22885e0c7c
Harley Araújo <harleyjm.araujo@gmail.com>
2021-04-11 22:49:53 -0300

    Update filefind.sh

--- filefind.sh
+++ filefind.sh
@@ -3,7 +3,7 @@ if [ "$1" == "" ]
 then
 echo "########### LIL DIX - FILEFIND ###########"
 echo "- - - - - -  - - -  - - - - - - - - - - - - -"
-echo "Example: $0 target.com pdf"
+echo "Example: $0 target.com php"
 else
 lynx -dump "http://google.com/search?num=100&q=site:"$1"+ext:"$2"" | grep ".$2" | cut -d "=" -f2 | egrep -v "site|google" | sed s'/...$//'g
 fi
